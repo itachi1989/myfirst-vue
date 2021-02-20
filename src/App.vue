e4f62c891280c915ee864eed14262d44053f3bb4
itachi <8072185+feiguang@user.noreply.gitee.com>
2021-02-20 11:15:45 +0800

    first push

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img v-bind:src="url" />
+    <ul>
+      <li v-for='(item,index) in arr'
+          :key='index'>
+        <input type="checkbox"
+               :value='index'
+               v-model="a" />
+        {{item}} {{index}}
+      </li>
+    </ul>
+
+    {{a}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      url: require("./assets/u.png"),
+      a: [],
+      arr: ['a', 'b', 'c']
+    }
+  }
+}
+</script>
+
+<style>
+</style>
